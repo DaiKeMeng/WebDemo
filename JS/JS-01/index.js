a148f0f3475f384b94046619e373baaab2a548c1
gongsi <393977117@qq.com>
2017-04-05 18:26:20 +0800

    提交项目

---
+++ JS/JS-01/index.js
@@ -0,0 +1,8 @@
+/**
+ * Created by daikemeng on 2017/3/30.
+ */
+// 如果js文件中两个方法名字一摸一样的话，最下面的方法覆盖上面的方法
+function changeTextAction() {
+    document.getElementById("demo1").innerHTML = "好你妹啊";
+    document.getElementById("demo2").innerHTML = "缺女朋友";
+}
